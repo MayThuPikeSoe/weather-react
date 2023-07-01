b011b8399a6e43fb665afd32db0dc11577823658
May Thu Pike Soe <maythupikesoe@gmail.com>
2023-06-30 22:44:25 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Weather from "./Weather";
+
+const App = () => {
+  return (
+    <div className="box">
+      <Weather />
+      <a href=" https://github.com/MayThuPikeSoe/weather-react.git">
+        GitHub Link
+      </a>
+    </div>
+  );
+};
+
+export default App;
